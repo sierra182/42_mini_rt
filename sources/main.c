e15590121e6659a4798befcd1c1095c946d6971f
sierra182 <sierra-echo@msn.com>
2024-04-26 14:28:38 +0200

    add init mlx

--- sources/main.c
+++ sources/main.c
@@ -10,12 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "ft_printf.h"
 #include "libft.h"
+#include "mlx.h"
+
+#define WIDTH 600
+#define HEIGHT 500
 
 int	main(void)
-{
-	ft_strlen("oui");
+{	
 	ft_printf("nique toi\n");
+	t_mlx	mlx;
+
+	mlx.connect = mlx_init();
+	if (!mlx.connect)
+		return (1);
+	mlx.window = mlx_new_window(mlx.connect, WIDTH, HEIGHT, "fdf");
+	if (!mlx.window)
+		return (mlx_destroy_display(mlx.connect), free(mlx.connect), 1);	
+	mlx_destroy_window(mlx->connect, mlx->window);
+	mlx_destroy_display(mlx->connect);
+	free(mlx->connect);
 	return (0);
 }
