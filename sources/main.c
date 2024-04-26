540d513a93accb08b5d2b1a8d7bcfce48ebd6ea2
sierra182 <sierra-echo@msn.com>
2024-04-26 14:51:25 +0200

    wip win

--- sources/main.c
+++ sources/main.c
@@ -24,6 +24,34 @@ typedef struct s_mlx
 	void	*window;
 }	t_mlx;
 
+int	key_press(int keycode, void *param[])
+{
+	// t_event	*event;
+
+	// event = (t_event *) param[1];
+	// event->flag = 1;
+	// key_press2(keycode, event);
+	// key_press3(keycode, event);
+	// if (keycode == 102)
+	// 	event->scl_z += 0.1;
+	// else if (keycode == 103)
+	// 	event->scl_z -= 0.1;
+	// else if (keycode == 115)
+	// 	event->scl += 1;
+	// else if (keycode == 100)
+	// 	event->scl -= 1;
+	// else if (keycode == 65432)
+	// 	event->scl_end += 0.1;
+	// else if (keycode == 65430)
+	// 	event->scl_end -= 0.1;
+	// else if (keycode == 114)
+	// 	init_event(event, (t_point **) param[0]);
+	// else 
+	if (keycode == 65307)
+		mlx_loop_end(param[2]);
+	return (0);
+}
+
 int	main(void)
 {	
 	ft_printf("nique toi\n");
@@ -34,9 +62,11 @@ int	main(void)
 		return (1);
 	mlx.window = mlx_new_window(mlx.connect, WIDTH, HEIGHT, "fdf");
 	if (!mlx.window)
-		return (mlx_destroy_display(mlx.connect), free(mlx.connect), 1);	
-	mlx_destroy_window(mlx->connect, mlx->window);
-	mlx_destroy_display(mlx->connect);
-	free(mlx->connect);
+		return (mlx_destroy_display(mlx.connect), free(mlx.connect), 1);
+	mlx_hook(mlx.window, 2, 1L << 0, key_press, (void *)  mlx.connect);
+	mlx_loop(mlx.connect);
+	mlx_destroy_window(mlx.connect, mlx.window);
+	mlx_destroy_display(mlx.connect);
+	free(mlx.connect);
 	return (0);
 }
