b96652ed2c5d2d311b26089e6440c85ab48f895c
sierra182 <sierra-echo@msn.com>
2024-05-22 11:40:55 +0200

    wip interface

--- sources/rays/rays.c
+++ sources/rays/rays.c
@@ -326,13 +326,13 @@ void	put_pxl_alpha(t_mlx *mlx, int x, int y, unsigned int alpha_color, void *img
 	if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
 	{
 		pxl_pos = x * mlx->img.bpp * inverse_eight + y * mlx->img.line_len;
-		int pxl_pos2 = (x - (WIDTH - 412))  * bpp * inverse_eight + (y - (HEIGHT - 200)) * line_len;
+		int pxl_pos2 = (x - (WIDTH - 762))  * bpp * inverse_eight + (y - (HEIGHT - 200)) * line_len;
 		if (*(unsigned int *)(img_data + pxl_pos2) == 0x37A75D
 			|| *(unsigned int *)(img_data + pxl_pos2) == 0x449A61
 			|| *(unsigned int *)(img_data + pxl_pos2) == 0x6D863B
 			|| *(unsigned int *)(img_data + pxl_pos2) == 0x4A9463
-			|| *(unsigned int *)(img_data + pxl_pos2) == 0x67744D
-			|| *(unsigned int *)(img_data + pxl_pos2) == 0x67754C)
+			|| *(unsigned int *)(img_data + pxl_pos2) == 0x34362E)
+			// || *(unsigned int *)(img_data + pxl_pos2) == 0x67754C)
 			
 		{			
 			*(unsigned int *)(mlx->img.img_data + pxl_pos) = 
@@ -360,7 +360,7 @@ void	launch_rays(t_mlx *mlx, t_data *data)
 		while (++x < data->cam.resol[0])
 		{			
 			exec_launch_rays(mlx, data, x, y);
-			if (x >= WIDTH - 412 && y >= HEIGHT - 200)
+			if (x >= WIDTH - 762 && y >= HEIGHT - 200)
 				add_xpm(mlx, x, y, img);
 		}
 		
