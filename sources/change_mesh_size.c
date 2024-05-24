7aa8d0208a4edde115c315890d4ced78a28644da
Dan SYLVAIN <dansylvain84@gmail.com>
2024-05-24 18:21:57 +0200

    sphere resizing ok

--- sources/change_mesh_size.c
+++ sources/change_mesh_size.c
@@ -13,19 +13,39 @@ void	chang_mech_size(t_data *data, int keycode)
 	if (keycode == PLUS)
 	{
 		if (mesh->actual_mesh.type == O_SP)
-			printf("sphere PLUS\n");
+		{
+			printf("sphere PLUS: %f\n", (((t_sphere *) mesh->actual_mesh.ref)->diameter));
+			((t_sphere *) mesh->actual_mesh.ref)->diameter += 1;
+			((t_sphere *) mesh->actual_mesh.ref)->square_radius = ((t_sphere *) mesh->actual_mesh.ref)->diameter / 2 * ((t_sphere *) mesh->actual_mesh.ref)->diameter / 2;
+		}
 		else if (mesh->actual_mesh.type == O_CY && mesh->ctrl_ispressed)
-			printf("cylinder PLUS + CTRL\n");
+		{
+			printf("cylinder PLUS: %f\n", (((t_cylinder *) mesh->actual_mesh.ref)->diameter));
+
+		}
 		else if (mesh->actual_mesh.type == O_CY)
-			printf("cylinder PLUS\n");
+		{
+			printf("cylinder PLUS: %f\n", (((t_cylinder *)  mesh->actual_mesh.ref)->diameter));
+
+		}
 	}
 	else if  (keycode == MINUS)
 	{
-		if (mesh->actual_mesh.type == O_SP)
-			printf("sphere MINUS\n");
+		if (mesh->actual_mesh.type == O_SP && ((t_sphere *) mesh->actual_mesh.ref)->diameter > 1)
+		{
+			printf("sphere MINUS: %f\n", (((t_sphere *) mesh->actual_mesh.ref)->diameter));
+			((t_sphere *) mesh->actual_mesh.ref)->diameter -= 1;
+			((t_sphere *) mesh->actual_mesh.ref)->square_radius = ((t_sphere *) mesh->actual_mesh.ref)->diameter / 2 * ((t_sphere *) mesh->actual_mesh.ref)->diameter / 2;
+		}
 		else if (mesh->actual_mesh.type == O_CY && mesh->ctrl_ispressed)
-			printf("cylinder MINUS + CTRL\n");
+		{
+			printf("cylinder MINUS + CTRL: %f\n", (((t_cylinder *)  mesh->actual_mesh.ref)->diameter));
+
+		}
 		else if (mesh->actual_mesh.type == O_CY)
-			printf("cylinder MINUS\n");
+		{
+			printf("cylinder MINUS: %f\n", (((t_cylinder *)  mesh->actual_mesh.ref)->diameter));
+
+		}
 	}
 }
