965640f037b5fbbfbba72b245d33f501049bf949
Dan SYLVAIN <dansylvain84@gmail.com>
2024-05-25 07:00:17 +0200

    funcs meshs resizing refactored

--- sources/change_mesh_size.c
+++ sources/change_mesh_size.c
@@ -2,8 +2,11 @@
 # include "mlx.h"
 # include "libft.h"
 
+void	handle_minus_key(t_data *data, int keycode);
+void	handle_plus_key(t_data *data, int keycode);
+
 /**========================================================================
- *                           SECTION HEADER
+ *                           chang_mech_size
  *========================================================================**/
 void	chang_mech_size(t_data *data, int keycode)
 {
@@ -11,48 +14,65 @@ void	chang_mech_size(t_data *data, int keycode)
 	mesh = &data->event;
 
 	if (keycode == PLUS)
-	{
-		if (mesh->actual_mesh.type == O_SP)
-		{
-			printf("sphere PLUS: %f\n", (((t_sphere *) mesh->actual_mesh.ref)->diameter));
-			((t_sphere *) mesh->actual_mesh.ref)->diameter += 1;
-			((t_sphere *) mesh->actual_mesh.ref)->square_radius = ((t_sphere *) mesh->actual_mesh.ref)->diameter / 2 * ((t_sphere *) mesh->actual_mesh.ref)->diameter / 2;
-		}
-		else if (mesh->actual_mesh.type == O_CY && mesh->ctrl_ispressed)
-		{
-
-			printf("cylinder PLUS: %f\n", (((t_cylinder *) mesh->actual_mesh.ref)->diameter));
-			((t_cylinder *) mesh->actual_mesh.ref)->height += 1;
-		}
-		else if (mesh->actual_mesh.type == O_CY && !mesh->ctrl_ispressed)
-		{
-			printf("cylinder PLUS: %f\n", (((t_cylinder *)  mesh->actual_mesh.ref)->diameter));
-			((t_cylinder *) mesh->actual_mesh.ref)->diameter += 1;
-			((t_cylinder *) mesh->actual_mesh.ref)->square_radius = ((t_cylinder *) mesh->actual_mesh.ref)->diameter / 2 * ((t_cylinder *) mesh->actual_mesh.ref)->diameter / 2;
-			((t_cylinder *) mesh->actual_mesh.ref)->radius = ((t_cylinder *) mesh->actual_mesh.ref)->diameter / 2;
-
-		}
-	}
+		handle_plus_key(data, keycode);
 	else if  (keycode == MINUS)
-	{
-		if (mesh->actual_mesh.type == O_SP && ((t_sphere *) mesh->actual_mesh.ref)->diameter > 1)
-		{
-			printf("sphere MINUS: %f\n", (((t_sphere *) mesh->actual_mesh.ref)->diameter));
-			((t_sphere *) mesh->actual_mesh.ref)->diameter -= 1;
-			((t_sphere *) mesh->actual_mesh.ref)->square_radius = ((t_sphere *) mesh->actual_mesh.ref)->diameter / 2 * ((t_sphere *) mesh->actual_mesh.ref)->diameter / 2;
-		}
-		else if (mesh->actual_mesh.type == O_CY && mesh->ctrl_ispressed && ((t_cylinder *) mesh->actual_mesh.ref)->height > 1)
-		{
-			printf("cylinder MINUS + CTRL: %f\n", (((t_cylinder *)  mesh->actual_mesh.ref)->diameter));
-			((t_cylinder *) mesh->actual_mesh.ref)->height -= 1;
-		}
-		else if (mesh->actual_mesh.type == O_CY && !mesh->ctrl_ispressed && ((t_cylinder *) mesh->actual_mesh.ref)->diameter > 1)
-		{
-			printf("cylinder MINUS: %f\n", (((t_cylinder *)  mesh->actual_mesh.ref)->diameter));
-			((t_cylinder *) mesh->actual_mesh.ref)->diameter -= 1;
-			((t_cylinder *) mesh->actual_mesh.ref)->square_radius = ((t_cylinder *) mesh->actual_mesh.ref)->diameter / 2 * ((t_cylinder *) mesh->actual_mesh.ref)->diameter / 2;
-			((t_cylinder *) mesh->actual_mesh.ref)->radius = ((t_cylinder *) mesh->actual_mesh.ref)->diameter / 2;
+		handle_minus_key(data, keycode);
+}
 
-		}
+/**========================================================================
+ *                           handle_plus_key
+ *========================================================================**/
+void	handle_plus_key(t_data *data, int keycode)
+{
+	t_event *mesh;
+	mesh = &data->event;
+	t_sphere *sphere;
+	t_cylinder *cyl;
+	t_obj		obj;
+
+	obj = mesh->actual_mesh;
+
+	sphere = (t_sphere *)obj.ref;
+	cyl = (t_cylinder *)obj.ref;
+	if (obj.type == O_SP)
+	{
+		(sphere)->diameter += 1;
+		(sphere)->square_radius = pow(sphere->diameter / 2, 2);
+	}
+	else if (obj.type == O_CY && mesh->ctrl_ispressed)
+		cyl->height += 1;
+	else if (obj.type == O_CY && !mesh->ctrl_ispressed)
+	{
+		cyl->diameter += 1;
+		cyl->square_radius = cyl->diameter / 2 * cyl->diameter / 2;
+		cyl->radius = cyl->diameter / 2;
+	}
+}
+/**========================================================================
+ *                           handle_minus_key
+ *========================================================================**/
+void	handle_minus_key(t_data *data, int keycode)
+{
+	t_event *mesh;
+	mesh = &data->event;
+	t_sphere *sphere;
+	t_cylinder *cyl;
+	t_obj		obj;
+
+	obj = mesh->actual_mesh;
+	sphere = (t_sphere *)obj.ref;
+	cyl = (t_cylinder *)obj.ref;
+	if (obj.type == O_SP && (sphere)->diameter > 1)
+	{
+		(sphere)->diameter -= 1;
+		(sphere)->square_radius = pow(sphere->diameter / 2, 2);
+	}
+	else if (obj.type == O_CY && mesh->ctrl_ispressed && cyl->height > 1)
+		cyl->height -= 1;
+	else if (obj.type == O_CY && !mesh->ctrl_ispressed && cyl->diameter > 1)
+	{
+		cyl->diameter -= 1;
+		cyl->square_radius = cyl->diameter / 2 * cyl->diameter / 2;
+		cyl->radius = cyl->diameter / 2;
 	}
 }
