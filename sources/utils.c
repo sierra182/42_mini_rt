21c53faa99320b3b350b69099012c4a81c2089b8
Dan SYLVAIN <dansylvain84@gmail.com>
2024-05-26 08:38:08 +0200

    norminated utils.c

--- sources/utils.c
+++ sources/utils.c
@@ -12,7 +12,7 @@ void	display_error(char *str)
 		perror("display_error");
 }
 
-void write_fd(int fd, char *str)
+void	write_fd(int fd, char *str)
 {
 	if (write (fd, str, ft_strlen(str)) == -1)
 		perror("write_fd_error");
