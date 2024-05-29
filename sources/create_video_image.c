b38aa71bbae5885bbf8c4d8133d6bc2dca3e3fcf
Dan SYLVAIN <dansylvain84@gmail.com>
2024-05-29 12:02:20 +0200

    added solar_system gif

--- sources/create_video_image.c
+++ sources/create_video_image.c
@@ -22,20 +22,8 @@ void	video_rotate_element(t_sphere *sphere)
 	
 	
 		init_matrix(trsf_matrix);
-		set_matrix_rotation(trsf_matrix, 7.5, axe);
+		set_matrix_rotation(trsf_matrix, 5, axe);
 		apply_matrix(trsf_matrix, &sphere->origin_vect, &applied_vect);
 		sphere->origin_vect = applied_vect;
-
-	// axe[0] = 0;
-	// axe[1] = 0;
-	// axe[2] = 1;
-	
-	// 	init_matrix(trsf_matrix);
-	// 	set_matrix_rotation(trsf_matrix, 7.5, axe);
-	// 	apply_matrix(trsf_matrix, &sphere->origin_vect, &applied_vect);
-	// 	sphere->origin_vect = applied_vect;
-	// 	 printf("another i: %i\n", i);
-
-
 
 }
