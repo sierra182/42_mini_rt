0c01b47d1f0bd3d6fdf332afc136fcafb95de279
Dan SYLVAIN <dansylvain84@gmail.com>
2024-05-30 14:40:51 +0200

    all funcs divided in diff files

--- sources/rays/get_cylinder_color.c
+++ sources/rays/get_cylinder_color.c
@@ -18,83 +18,13 @@ void	color_with_light(t_color *mesh_color,
 void	limit_to_255(t_color *color);
 int	get_cylinder_color_discs(t_get_color_params *params);
 int	is_cylinder_surface_between( t_cylinder *cyl, t_ray_vector *normal, double mesh[]);
+int is_in_cyl_height(t_ray_vector *normal, t_cylinder *cyl, double mesh[]);
+
+
 
 /**========================================================================
  *                           GET_CYLINDER_COLOR_CYL
-*========================================================================**/
-int	is_in_cyl_diam( t_cylinder *cyl, t_ray_vector *normal, double mesh[])
-{
-	t_ray_vector	subt_vect;
-	t_ray_vector	inter_vect;
-	
-	t_ray			ray;
-	cast_vector_mat_ray(&cyl->axis_vect, &ray.dir_vect);
-	cast_vector_mat_ray(&cyl->origin_vect, &ray.origin_vect);
-	subtract_vector(mesh, cyl->origin_vect.axis, subt_vect.axis);
-		 normalize_vector(ray.dir_vect.axis);
-	 scale_vector(ray.dir_vect.axis, scalar_product(ray.dir_vect.axis, subt_vect.axis), inter_vect.axis);
-	subtract_vector(inter_vect.axis, subt_vect.axis, subt_vect.axis);
-	if (get_vector_magnitude(subt_vect.axis) <= cyl->diameter * 0.5)
-		return (1);
-	else
-		return (0);
-}
-
-void	print_delay(t_ray_vector *subt_vect, double diam)
-{
-	static int delay;
-
-	delay = (delay + 1) % 1800;
-	if (!delay)
-		printf("mag:%f rayon:%f\n", get_vector_magnitude(subt_vect->axis), diam * 0.5 );
-}
-
-int	is_cylinder_surface_between2( t_cylinder *cyl, t_ray_vector *normal, double mesh[])
-{
-	t_ray_vector	subt_vect;
-	t_ray_vector	inter_vect;
-	
-	t_ray			ray;
-	cast_vector_mat_ray(&cyl->axis_vect, &ray.dir_vect);
-	cast_vector_mat_ray(&cyl->origin_vect, &ray.origin_vect);
-	subtract_vector(mesh, cyl->origin_vect.axis, subt_vect.axis);
-	normalize_vector(ray.dir_vect.axis);
-	scale_vector(ray.dir_vect.axis,  scalar_product(ray.dir_vect.axis, subt_vect.axis), inter_vect.axis);
-	subtract_vector(inter_vect.axis, subt_vect.axis, subt_vect.axis);	
-		return ((get_vector_magnitude(subt_vect.axis) >= cyl->diameter * 0.5
-	&& cyl->which_t == 2) || ((get_vector_magnitude(subt_vect.axis) <= cyl->diameter * 0.5
-	&& cyl->which_t == 1)));
-}
-int is_in_cyl_height(t_ray_vector *normal, t_cylinder *cyl, double mesh[])
-{
-	t_ray_vector	subt_vect;
-
-	subtract_vector(mesh, cyl->origin_vect.axis, subt_vect.axis);
-	if (fabs(scalar_product(normal->axis, subt_vect.axis)) < cyl->height * 0.5)	
-	{
-		return (1);
-	}	
-	return (0);
-}
-
-int	is_in_cylinder(t_ray_vector *normal, t_cylinder *cyl, double mesh[])
-{
-	return (is_in_cyl_height(normal, cyl, mesh)
-		&& is_in_cyl_diam(cyl, normal, mesh));
-}
-int	are_light_and_cam_in_different_cyl_space(t_ray_vector *normal, t_spotlight *light, t_cylinder *cyl, t_cam *cam)
-{
-	if (is_in_cylinder(normal, cyl, light->origin_vect.axis)
-		&& is_in_cylinder(normal, cyl, cam->origin_vect.axis))	
-		return (0);
-	if (!is_in_cylinder(normal, cyl, light->origin_vect.axis)
-		&& !is_in_cylinder(normal, cyl, cam->origin_vect.axis))	
-		return (0);
-	return (1);
-}
-
-
-
+ *========================================================================**/
 int	get_cylinder_color_cyl(t_get_color_params *params)
 {
 	t_ray_vector	normal;
