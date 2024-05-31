0adb12f3305a4daaf3cba93e25d1193adc3acb5e
Dan SYLVAIN <dansylvain84@gmail.com>
2024-05-31 12:39:24 +0200

    first success with translation

--- sources/meta/create_video_image.c
+++ sources/meta/create_video_image.c
@@ -2,6 +2,8 @@
 # include "x_matrix.h"
 
 void	rotate_mesh(t_matrix_vector *vect, double angle, int axe[]);
+void	trsl_mesh(t_cam *cam, t_matrix_vector *vect, double values[]);
+double	*symmetrize_vector(double vect[]);
 
 
 /**========================================================================
@@ -17,6 +19,17 @@ void	video_rotate_element(t_sphere *sphere, int obj_num)
 	t_matrix_vector applied_vect;
 	double angle = 5;
 
+	t_ray_vector point;
+	t_ray_vector point2;
+
+	point.axis[0] = 10;
+	point.axis[1] = 0;
+	point.axis[2] = 0;
+	
+	
+	trsl_mesh(NULL, &sphere->origin_vect, point.axis);
+
+
 	axe[0] = 1;
 	axe[1] = 0;
 	axe[2] = 0;
@@ -29,4 +42,8 @@ void	video_rotate_element(t_sphere *sphere, int obj_num)
 	axe[1] = 0;
 	axe[2] = 1;
 	rotate_mesh(&sphere->origin_vect, angle, axe);
+
+	symmetrize_vector(point.axis);
+	trsl_mesh(NULL, &sphere->origin_vect, point.axis);
+
 }
