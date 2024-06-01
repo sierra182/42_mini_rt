7c57469e58c7b8ff3069687d671d350df1943044
Dan SYLVAIN <dansylvain84@gmail.com>
2024-06-01 09:07:18 +0200

    changed script

--- generate_bin_files.sh
+++ generate_bin_files.sh
@@ -2,54 +2,28 @@
 
 export AUTOMATED_TEST=true
 
-# Nombre total de fichiers à traiter
 total_files=$(ls tests/rt_files/*.rt | wc -l)
-
 if [ "$total_files" -eq 0 ]; then
     exit 1
 fi
-
-# Demander à l'utilisateur à partir de quelle valeur commencer la numérotation des fichiers .bin
 read -p "À partir de quelle valeur voulez-vous commencer la numérotation des fichiers .bin ? " start_index
-
-# Vérifier si la valeur saisie est un nombre entier
 if ! [[ "$start_index" =~ ^[0-9]+$ ]]; then
     echo "La valeur saisie n'est pas un nombre entier. Sortie du script."
     exit 1
 fi
-
-
-# Boucle pour traiter chaque fichier
 for ((i=0; i<total_files; i++)); do
-    # Chemin du fichier .rt
     rt_file="tests/rt_files/file_$i.rt"
-    # Chemin du fichier .bin de référence
     ref_bin_file="tests/bin_files/file_$i.bin"
-
-
-	
 	new_bin_file="file_$((start_index + i)).bin"
 	bin_folder="tests/new_files"
-
-    # Commande pour exécuter miniRT avec le fichier .rt
     ./miniRT "$rt_file" &
-    # Attendre 1 seconde pour s'assurer que le programme s'est ouvert
     sleep 0.5
-
-    # Attendre que le fichier .bin soit généré
     while [ ! -f "file_0.bin" ]; do
         sleep 0.5
     done
-
-    # Comparer le fichier .bin généré avec le fichier de référence
     mv "file_0.bin" "$new_bin_file"
-
-    # Déplacer le fichier .bin renommé vers le dossier de destination
     mv "$new_bin_file" "$bin_folder/"
-
     pkill miniRT
-
     echo "$new_bin_file ✅ : bin file created and moved"
 done
-
 echo "All files processed successfully."
--- test_script.sh
+++ test_script.sh
@@ -1,44 +1,24 @@
 #!/bin/bash
 
 export AUTOMATED_TEST=true
-
-# Nombre total de fichiers à traiter
 total_files=$(ls tests/rt_files/*.rt | wc -l)
-
 exit_code=0
-
-# Boucle pour traiter chaque fichier
 for ((i=0; i<total_files; i++)); do
-    # Chemin du fichier .rt
     rt_file="tests/rt_files/file_$i.rt"
-    # Chemin du fichier .bin de référence
     ref_bin_file="tests/bin_files/file_$i.bin"
-
-
-    # Commande pour exécuter miniRT avec le fichier .rt
     ./miniRT "$rt_file" &
-    # Attendre 1 seconde pour s'assurer que le programme s'est ouvert
     sleep 0.5
-
-    # Attendre que le fichier .bin soit généré
     while [ ! -f "file_0.bin" ]; do
         sleep 0.5
     done
-
-    # Comparer le fichier .bin généré avec le fichier de référence
     if cmp -s "file_0.bin" "$ref_bin_file"; then
         echo "file_$i ✅ : no difference found"
     else
         echo "file_$i ❌ : files are different"
 		exit_code=1
     fi
-
-    # Nettoyer le fichier .bin généré
     rm "file_0.bin"
 	sleep 0.5
     pkill miniRT
-
 done
-
-# Retourner le code de sortie approprié
 exit $exit_code
