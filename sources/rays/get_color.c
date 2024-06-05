3068b78d9ec82016e1271ffd5b9050163d8fea64
sierra182 <sierra-echo@msn.com>
2024-06-05 09:55:47 +0200

    verif normal

--- sources/rays/get_color.c
+++ sources/rays/get_color.c
@@ -133,11 +133,10 @@ void	compute_pl_normal_and_light_ray(t_get_color_params *params,
 	double view_dot_normal;
 
 	cast_vector_mat_ray(&plane->norm_vect, normal);
+	normalize_vector(normal->axis);
 	get_intersect_point(params->ray, params->t, &light_ray->origin_vect);
 	subtract_vector(params->data->spotlight.origin_vect.axis,
 		light_ray->origin_vect.axis, light_ray->dir_vect.axis);
-	normalize_vector(params->ray->dir_vect.axis);
-	normalize_vector(normal->axis);
 	view_dot_normal = scalar_product(normal->axis, params->ray->dir_vect.axis);
 	if (view_dot_normal > 0)
 		symmetrize_vector(normal->axis);
