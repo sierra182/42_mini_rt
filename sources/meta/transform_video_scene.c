bf5f71c8952866567823262f7c3fd6b3ee91c0b5
Dan SYLVAIN <dansylvain84@gmail.com>
2024-06-05 12:33:13 +0200

    pendulum pendelt richtig

--- sources/meta/transform_video_scene.c
+++ sources/meta/transform_video_scene.c
@@ -27,62 +27,65 @@ void	video_rotate_cogs(t_cylinder *cyl, int	i, int cyl_nbr)
 	int axe[3];
 	t_ray_vector point;
 	double angle = -3.33;
-	
+	static int l = 0;
 	static int k = 0;
 	static int j = 1;
-	if (k - cyl_nbr * 5 == 0)
+	if (l == 16)
 	{
+		l = 0;
 		j = -j;
 	}
 
 	
-	if (i < 24)
-	{
-		angle = 2;
-		axe[0] = 0;
-		axe[1] = 1;
-		axe[2] = 0;
-		rotate_mesh(&cyl->axis_vect, angle, axe);
-	}
-	else if (i < 40)
-	{
-		axe[0] = 0;
-		axe[1] = 1;
-		axe[2] = 0;
-		rotate_mesh(&cyl->axis_vect, angle, axe);
-	}
-	else if (i < 56)
-	{
-		axe[0] = 0;
-		axe[1] = 0;
-		axe[2] = 1;
-		rotate_mesh(&cyl->axis_vect, angle, axe);
-	}
-	else
-	{
-		angle = 2;
+	// if (i < 24)
+	// {
+	// 	angle = 2;
+	// 	axe[0] = 0;
+	// 	axe[1] = 1;
+	// 	axe[2] = 0;
+	// 	rotate_mesh(&cyl->axis_vect, angle, axe);
+	// }
+	// else if (i < 40)
+	// {
+	// 	axe[0] = 0;
+	// 	axe[1] = 1;
+	// 	axe[2] = 0;
+	// 	rotate_mesh(&cyl->axis_vect, angle, axe);
+	// }
+	// else if (i < 56)
+	// {
+	// 	axe[0] = 0;
+	// 	axe[1] = 0;
+	// 	axe[2] = 1;
+	// 	rotate_mesh(&cyl->axis_vect, angle, axe);
+	// }
+	// else
+	// {
+		angle = -3.33;
 		if (j == -1)
-			angle = -2;
+			angle = 3.33;
 
-		point.axis[0] = 0;
-		point.axis[1] = 60;	
-		point.axis[2] = 0;
+		point.axis[0] = cyl->axis_vect.axis[0];
+		point.axis[1] = cyl->axis_vect.axis[1] - 60;	
+		point.axis[2] = cyl->axis_vect.axis[2];
 
+		trsl_mesh(NULL, &cyl->origin_vect, point.axis);
 
 		axe[0] = 1;
 		axe[1] = 0;
 		axe[2] = 0;
 		rotate_mesh(&cyl->axis_vect, angle, axe);
-		axe[0] = 0;
-		axe[1] = 1;
-		axe[2] = 0;
-		rotate_mesh(&cyl->axis_vect, angle, axe);
-		axe[0] = 0;
-		axe[1] = 0;
-		axe[2] = 1;
-		rotate_mesh(&cyl->axis_vect, angle, axe);
-
-	}
+		// axe[0] = 0;
+		// axe[1] = 1;
+		// axe[2] = 0;
+		// rotate_mesh(&cyl->axis_vect, angle, axe);
+		// axe[0] = 0;
+		// axe[1] = 0;
+		// axe[2] = 1;
+		// rotate_mesh(&cyl->axis_vect, angle, axe);
+		symmetrize_vector(point.axis);
+		trsl_mesh(NULL, &cyl->origin_vect, point.axis);
+	// }
 
 		
 	
@@ -90,7 +93,7 @@ void	video_rotate_cogs(t_cylinder *cyl, int	i, int cyl_nbr)
 
 	
 	k++;
-
+	l++;
 
 
 }
@@ -103,7 +106,7 @@ void	rotate_pendulum(t_sphere *sphere, int i)
 	static double angle = -2;
 
 	printf("i: %i\n", i);
-	if (j == 16)
+	if (j == 8)
 	{
 		j = 0;
 		angle = -angle;
