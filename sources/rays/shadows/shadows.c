4cf04c2583d9244f0a2020b5832b9fe936c7ac2a
sierra182 <sierra-echo@msn.com>
2024-06-07 10:39:23 +0200

    wip norm

--- sources/rays/shadows/shadows.c
+++ sources/rays/shadows/shadows.c
@@ -1,5 +1,5 @@
-# include "se_mini_struct.h"
-# include "x_linear_algebra.h"
+#include "se_mini_struct.h"
+#include "x_linear_algebra.h"
 
 void	get_local_intersect_point(t_ray *ray, double t, t_ray_vector *inter_pt);
 double	is_intersect_plane(t_ray *ray, void *input_plane, t_ray_vector *i);
@@ -16,22 +16,22 @@ int	has_sphere_shadow(t_data *data, t_obj *mesh, t_ray *light_rays[])
 	double			mesh_mag;
 	double			light_mag;
 	t_ray_vector	inter_pt;
-		
+
 	i = -1;
 	while (++i < data->sp_nbr)
-	{	
+	{
 		if (mesh->ref && (void *) &data->spheres[i] != mesh->ref)
 		{
-			t = is_intersect_sphere(light_rays[1], &data->spheres[i], NULL);	
+			t = is_intersect_sphere(light_rays[1], &data->spheres[i], NULL);
 			if (t)
 			{
 				get_local_intersect_point(light_rays[1], t, &inter_pt);
 				mesh_mag = get_vector_magnitude(inter_pt.axis);
-				light_mag = get_vector_magnitude(light_rays[0]->dir_vect.axis);						
+				light_mag = get_vector_magnitude(light_rays[0]->dir_vect.axis);
 				if (mesh_mag - 1e-5 < light_mag)
 					return (1);
 			}
-		}		
+		}
 	}
 	return (0);
 }
@@ -50,14 +50,14 @@ int	has_cylinder_shadow(t_data *data, t_obj *mesh, t_ray *light_rays[])
 		if (mesh->ref && (void *) & data->cylinders[i] != mesh->ref)
 		{
 			t = is_intersect_cylinder(light_rays[1], &data->cylinders[i],
-				NULL);	
+					NULL);
 			if (t)
 			{
 				get_local_intersect_point(light_rays[1], t, &inter_pt);
 				mesh_mag = get_vector_magnitude(inter_pt.axis);
-				light_mag = get_vector_magnitude(light_rays[0]->dir_vect.axis);					
+				light_mag = get_vector_magnitude(light_rays[0]->dir_vect.axis);
 				if (mesh_mag - 1e-5 < light_mag)
-					return (1);		
+					return (1);
 			}
 		}
 	}
@@ -71,7 +71,7 @@ int	has_plane_shadow(t_data *data, t_obj *mesh, t_ray *light_rays[])
 	double			mesh_mag;
 	double			light_mag;
 	t_ray_vector	inter_pt;
-	
+
 	i = -1;
 	while (++i < data->pl_nbr)
 	{
@@ -81,7 +81,7 @@ int	has_plane_shadow(t_data *data, t_obj *mesh, t_ray *light_rays[])
 			if (t)
 			{
 				get_local_intersect_point(light_rays[1], t, &inter_pt);
-				light_mag = get_vector_magnitude(light_rays[0]->dir_vect.axis);			
+				light_mag = get_vector_magnitude(light_rays[0]->dir_vect.axis);
 				mesh_mag = get_vector_magnitude(inter_pt.axis);
 				if (mesh_mag - 1e-5 < light_mag)
 					return (1);
@@ -93,19 +93,19 @@ int	has_plane_shadow(t_data *data, t_obj *mesh, t_ray *light_rays[])
 
 int	has_shadow(t_data *data, t_ray_vector *normal, t_obj *mesh,
 	t_ray *light_ray)
-{	
-	t_ray 		light_ray_norm;
-	t_ray		light_ray_cpy;
+{
+	t_ray	light_ray_norm;
+	t_ray	light_ray_cpy;
 
 	light_ray_cpy = *light_ray;
 	light_ray_norm = *light_ray;
-	normalize_vector(light_ray_norm.dir_vect.axis);			
+	normalize_vector(light_ray_norm.dir_vect.axis);
 	if (has_sphere_shadow(data, mesh,
-		(t_ray *[]){&light_ray_cpy, &light_ray_norm})
+			(t_ray *[]){&light_ray_cpy, &light_ray_norm})
 		|| has_cylinder_shadow(data, mesh,
-		(t_ray *[]){&light_ray_cpy, &light_ray_norm})
+			(t_ray *[]){&light_ray_cpy, &light_ray_norm})
 		|| has_plane_shadow(data, mesh,
-		(t_ray *[]){&light_ray_cpy, &light_ray_norm}))
-		return(1);
-	return(0);
+			(t_ray *[]){&light_ray_cpy, &light_ray_norm}))
+		return (1);
+	return (0);
 }
--- sources/rays/shadows/shadows_exclusion.c
+++ sources/rays/shadows/shadows_exclusion.c
@@ -1,69 +1,38 @@
-# include "se_mini_struct.h"
-# include "x_linear_algebra.h"
-
-// int	is_same_sphere_space(t_sphere *a, t_sphere *b)
-// {
-// 	return (is_equal_vector(a->origin_vect.axis, b->origin_vect.axis)
-// 		&& a->diameter == b->diameter);
-// }
-
-// int	is_same_plane_space(t_plane *a, t_plane *b)
-// {
-// 	return (is_equal_vector(a->origin_vect.axis, b->origin_vect.axis)
-// 		&& is_equal_vector(a->norm_vect.axis, b->norm_vect.axis));
-// }
-
-// int	is_same_cylinder_space(t_cylinder *a, t_cylinder *b)
-// {	
-// 	double	distance;
-// 	t_matrix_vector	subt_vect;
-// t_matrix_vector	tmp1;
-// t_matrix_vector	tmp2;
-// 	subtract_vector(a->origin_vect.axis, b->origin_vect.axis, subt_vect.axis);
-// 	distance = get_vector_magnitude(subt_vect.axis);
-// 	cross_product(&subt_vect, &a->axis_vect, &tmp1);
-// 	cross_product(&subt_vect, &b->axis_vect, &tmp2);
-// 	return (a->diameter == b->diameter
-// 		&& 
-// 		((is_equal_vector(a->origin_vect.axis, b->origin_vect.axis)
-// 	 	&& is_equal_vector(a->axis_vect.axis, b->axis_vect.axis))
-// 		|| ((are_collinear_vectors(&tmp1, 1e-4)
-// 		&& are_collinear_vectors(&tmp2, 1e-4) && distance > 0)
-// 		&& distance <= (a->height + b->height) * 0.5)
-// 		|| ((are_collinear_vectors(&a->axis_vect, 1e-4)
-// 		&& are_collinear_vectors(&b->axis_vect, 1e-4) && distance == 0)
-// 		))	
-// 		);
-// }
+#include "se_mini_struct.h"
+#include "x_linear_algebra.h"
 
 int	is_sphere_surface_between(t_sphere *sphere, t_spotlight *spotlight)
 {
 	t_matrix_vector	subt_vect;
+	double			sphere_light_mag;
 
 	subtract_vector(spotlight->origin_vect.axis, sphere->origin_vect.axis,
-		subt_vect.axis);	
-	return ((get_vector_magnitude(subt_vect.axis) > sphere->radius //!opti!!!
-		&& sphere->which_t == 2)
-		|| ((get_vector_magnitude(subt_vect.axis) < sphere->radius
-		&& sphere->which_t == 1)));
+		subt_vect.axis);
+	sphere_light_mag = get_vector_magnitude(subt_vect.axis);
+	return ((sphere_light_mag > sphere->radius
+			&& sphere->which_t == 2)
+		|| (sphere_light_mag < sphere->radius
+			&& sphere->which_t == 1));
 }
 
 int	is_cylinder_surface_between( t_cylinder *cyl, t_ray_vector *normal,
 	double mesh[])
 {
-	t_ray_vector	subt_vect;
-	t_ray_vector	inter_vect;	
 	t_ray			ray;
+	t_ray_vector	subt_vect;
+	t_ray_vector	inter_vect;
+	double			cyl_light_mag;
 
 	cast_vector_mat_ray(&cyl->axis_vect, &ray.dir_vect);
 	cast_vector_mat_ray(&cyl->origin_vect, &ray.origin_vect);
-	subtract_vector(mesh, cyl->origin_vect.axis, subt_vect.axis);	
+	subtract_vector(mesh, cyl->origin_vect.axis, subt_vect.axis);
 	normalize_vector(ray.dir_vect.axis);
-	scale_vector(ray.dir_vect.axis,  scalar_product(ray.dir_vect.axis,
-		subt_vect.axis), inter_vect.axis);
-	subtract_vector(inter_vect.axis, subt_vect.axis, subt_vect.axis);	
-	return ((get_vector_magnitude(subt_vect.axis) >= cyl->radius
-		&& cyl->which_t == 2)
-		|| ((get_vector_magnitude(subt_vect.axis) <= cyl->radius 
-		&& cyl->which_t == 1)));
+	scale_vector(ray.dir_vect.axis, scalar_product(ray.dir_vect.axis,
+			subt_vect.axis), inter_vect.axis);
+	subtract_vector(inter_vect.axis, subt_vect.axis, subt_vect.axis);
+	cyl_light_mag = get_vector_magnitude(subt_vect.axis);
+	return ((cyl_light_mag >= cyl->radius
+			&& cyl->which_t == 2)
+		|| (cyl_light_mag <= cyl->radius
+			&& cyl->which_t == 1));
 }
