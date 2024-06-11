e1a43eab63a6ef1cfcc0af2739d84c4fe06860bd
Dan SYLVAIN <dansylvain84@gmail.com>
2024-06-11 14:57:13 +0200

    multi spotlight handled

--- bonus/sources/parsing/all_necessary_elements_are_present_bonus.c
+++ bonus/sources/parsing/all_necessary_elements_are_present_bonus.c
@@ -12,7 +12,8 @@ int	all_necessary_elements_are_present(char *map_path)
 		return (0);
 	if (element_is_present(map_path, "C") != 1)
 		return (0);
-	if (element_is_present(map_path, "L") != 1)
+	nbr = element_is_present(map_path, "L");
+	if (nbr == 0)
 		return (0);
 	nbr = element_is_present(map_path, "sp");
 	if (nbr == 0)
