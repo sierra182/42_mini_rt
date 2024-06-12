911053f717403a3527b419cb48a89a16c0ed510a
Sebastien Vidot <svidot@c2r6p8.42perpignan.fr>
2024-06-12 11:33:51 +0200

    fix cond jump data is test

--- sources/init_data/init_data.c
+++ sources/init_data/init_data.c
@@ -60,6 +60,7 @@ int	init_data(char *map_path, t_data *data)
  *========================================================================**/
 void	init_vars(t_data *data)
 {
+	data->is_test = 0;
 	data->refresh = 1;
 	data->event.ctrl_ispressed = 0;
 	data->event.legend = 0;
