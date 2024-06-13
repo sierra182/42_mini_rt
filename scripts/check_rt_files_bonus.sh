e1b7e55bf4ec7b1430337488b1df8bcbd96475a3
Dan SYLVAIN <dansylvain84@gmail.com>
2024-06-13 11:43:17 +0200

    added script check_rt_files_bonus

---
+++ scripts/check_rt_files_bonus.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+total_files=$(ls tests/rt_files_bonus/*.rt 2>/dev/null | wc -l)
+if [ "$total_files" -eq 0 ]; then
+    echo "Aucun fichier .rt trouvé dans le répertoire tests/rt_files_bonus."
+    exit_code=0
+    exit 1
+fi
+
+for ((i=0; i<total_files; i++)); do
+    rt_file="tests/rt_files_bonus/file_$i.rt"
+    ./bonus/miniRT_bonus "$rt_file" &
+    sleep 0.5
+
+    while true; do
+		read -p "file_$i.rt	Appuyez sur espace ou entrée pour continuer, ou 'q' pour quitter...
+" -n 1 key
+    if [[ "$key" == " " || "$key" == "" ]]; then
+            pkill miniRT
+            break
+        elif [ "$key" == "q" ]; then
+            exit 0
+        fi
+    done
+done
+exit $exit_code
