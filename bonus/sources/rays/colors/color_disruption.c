43a17a7fbe21a930c95595ea45c7cd9e6c3e9c43
Dan SYLVAIN <dansylvain84@gmail.com>
2024-06-14 15:42:07 +0200

    func conversion uv to xy done

--- bonus/sources/rays/colors/color_disruption.c
+++ bonus/sources/rays/colors/color_disruption.c
@@ -1,6 +1,11 @@
 #include "get_sph_pl_bck_color_bonus.h"
 void	clamp_rgb_0(t_color *color);
 #include <math.h>
+void	modif_uv(t_get_color_params *params, t_ray_pack light_ray, int size);
+void calculate_uv(t_ray_vector point, double *u, double *v);
+void checker_color(double u, double v, int checker_size, t_color *color);
+
+#define TEXTURE_WIDTH 512
 
 // void cartesian_to_spherical(t_ray_vector *point, float *theta, float *phi)
 // {
@@ -20,6 +25,46 @@ void	clamp_rgb_0(t_color *color);
  void	modif_uv(t_get_color_params *params, t_ray_pack light_ray, int size);
 
  *========================================================================**/
+
+
+void uv_to_texture_coordinates(double u, double v, int *x, int *y) {
+    *x = (int)(u * TEXTURE_WIDTH);
+    *y = (int)(v * TEXTURE_WIDTH);
+
+    if (*x < 0) *x = 0;
+    if (*y < 0) *y = 0;
+    if (*x >= TEXTURE_WIDTH) *x = TEXTURE_WIDTH - 1;
+    if (*y >= TEXTURE_WIDTH) *y = TEXTURE_WIDTH - 1;
+	// printf("%f, %f => %i, %i\n", u, v, *x, *y);
+}
+
+
+
+void	modif_uv(t_get_color_params *params, t_ray_pack light_ray, int size)
+{
+	double u;
+	double v;
+	t_sphere		*sphere;
+
+	sphere = (t_sphere *) params->mesh->ref;
+	if (sphere->checkerboard == 0 && !sphere->bump_map_path)
+		return ;
+	calculate_uv(*params->normal, &u, &v);
+	if (sphere->checkerboard == 1)
+	{
+		checker_color(u, v, size, params->color);
+	}
+	int x, y;
+	uv_to_texture_coordinates(u, v, &x, &y);
+	// printf("bmp: %s\n", sphere->bump_map_path);
+}
+
+
+
+
+
+
+
 void calculate_uv(t_ray_vector point, double *u, double *v) {
 	double theta;
 	double phi;
@@ -44,21 +89,4 @@ void checker_color(double u, double v, int checker_size, t_color *color)
 		color->rgb[1] *= 0.3;
 		color->rgb[2] *= 0.3;
 	}
-}
-
-void	modif_uv(t_get_color_params *params, t_ray_pack light_ray, int size)
-{
-	double u;
-	double v;
-	t_sphere		*sphere;
-
-	sphere = (t_sphere *) params->mesh->ref;
-	if (sphere->checkerboard == 0 && !sphere->bump_map_path)
-		return ;
-	if (sphere->checkerboard == 1)
-	{
-		calculate_uv(*params->normal, &u, &v);
-		checker_color(u, v, size, params->color);
-	}
-	printf("bmp: %s\n", sphere->bump_map_path);
 }
