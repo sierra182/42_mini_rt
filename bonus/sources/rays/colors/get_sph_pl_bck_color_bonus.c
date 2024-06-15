f1822f844754c82c2222d4d14845220c73fc1936
Sebastien Vidot <svidot@c2r6p8.42perpignan.fr>
2024-06-15 15:05:13 +0200

    find bug sphere

--- bonus/sources/rays/colors/get_sph_pl_bck_color_bonus.c
+++ bonus/sources/rays/colors/get_sph_pl_bck_color_bonus.c
@@ -120,27 +120,3 @@ void	get_plane_color(t_get_color_params *params)
 	apply_aces_tonemap(params->color);
 }
 
-/**========================================================================
- *                           GET_BACKGROUND_COLOR
- *========================================================================**/
-int	get_background_color(t_ray *ray, t_data *data)
-{
-	int		color[2];
-	int		rgb[3];
-	double	dir;
-	t_color	*bg_color;
-	double	intensity;
-
-	intensity = data->ambiant_light.intensity;
-	bg_color = (t_color *)&data->ambiant_light.color;
-	dir = (ray->dir_vect.axis[1] + 1.0) * 0.5;
-	color[0] = get_color(intensity * 255, intensity * 255, intensity * 255);
-	color[1] = get_color(bg_color->rgb[0] * intensity, bg_color->rgb[1]
-			* intensity, bg_color->rgb[2] * intensity);
-	rgb[0] = (int)((1.0 - dir) * ((color[1] >> 16) & 0xFF) + dir
-			* ((color[0] >> 16) & 0xFF));
-	rgb[1] = (int)((1.0 - dir) * ((color[1] >> 8) & 0xFF) + dir
-			* ((color[0] >> 8) & 0xFF));
-	rgb[2] = (int)((1.0 - dir) * (color[1] & 0xFF) + dir * (color[0] & 0xFF));
-	return (rgb[0] << 16 | rgb[1] << 8 | rgb[2]);
-}
--- bonus/sources/rays/ray_utils_bonus.c
+++ bonus/sources/rays/ray_utils_bonus.c
@@ -40,3 +40,28 @@ void	compute_light_ray(t_spotlight *spotlight, t_get_color_params *params,
 	normalize_vector(light_ray->ray.dir_vect.axis, light_ray->magnitude,
 		light_ray->ray_norm.dir_vect.axis);
 }
+
+/**========================================================================
+ *                           GET_BACKGROUND_COLOR
+ *========================================================================**/
+int	get_background_color(t_ray *ray, t_data *data)
+{
+	int		color[2];
+	int		rgb[3];
+	double	dir;
+	t_color	*bg_color;
+	double	intensity;
+
+	intensity = data->ambiant_light.intensity;
+	bg_color = (t_color *)&data->ambiant_light.color;
+	dir = (ray->dir_vect.axis[1] + 1.0) * 0.5;
+	color[0] = get_color(intensity * 255, intensity * 255, intensity * 255);
+	color[1] = get_color(bg_color->rgb[0] * intensity, bg_color->rgb[1]
+			* intensity, bg_color->rgb[2] * intensity);
+	rgb[0] = (int)((1.0 - dir) * ((color[1] >> 16) & 0xFF) + dir
+			* ((color[0] >> 16) & 0xFF));
+	rgb[1] = (int)((1.0 - dir) * ((color[1] >> 8) & 0xFF) + dir
+			* ((color[0] >> 8) & 0xFF));
+	rgb[2] = (int)((1.0 - dir) * (color[1] & 0xFF) + dir * (color[0] & 0xFF));
+	return (rgb[0] << 16 | rgb[1] << 8 | rgb[2]);
+}
