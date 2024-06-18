9d0fa38fd80178bdff788e4c2eee9bd524d3d05d
Sebastien Vidot <svidot@c2r6p8.42perpignan.fr>
2024-06-18 14:44:27 +0200

    find bug antia

--- bonus/include/event/event_bonus.h
+++ bonus/include/event/event_bonus.h
@@ -3,7 +3,7 @@
 
 # include "se_mini_struct_bonus.h"
 
-void	new_ray(t_cam *cam, t_ray *ray, int x, int y);
+// void	new_ray(t_cam *cam, t_ray *ray, int x, int y);
 void	get_closest_intersection_sp(t_data *data, t_ray *ray, t_obj *obj);
 void	get_closest_intersection_cy(t_data *data, t_ray *ray, t_obj *obj);
 void	get_closest_intersection_pl(t_data *data, t_ray *ray, t_obj *obj);
--- bonus/include/event/event_light_bonus.h
+++ bonus/include/event/event_light_bonus.h
@@ -3,7 +3,8 @@
 
 # include "se_mini_struct_bonus.h"
 
-void	new_ray(t_cam *cam, t_ray *ray, int x, int y);
+// void	new_ray(t_cam *cam, t_ray *ray, int x, int y);
+void		new_ray(t_cam *cam, t_ray *ray, double x, double y);
 double	is_intersect_sphere(t_ray *ray, void *input_sphere, t_ray_vector *i);
 int		is_behind_cam(double t);
 
