57b30efb8169c0ae0b7e3f13208e8cb7492abcb7
Dan SYLVAIN <dansylvain84@gmail.com>
2024-06-22 18:14:17 +0200

    actual.mesh.type conditional jump fixed

--- bonus/sources/init_data/init_data_bonus.c
+++ bonus/sources/init_data/init_data_bonus.c
@@ -80,6 +80,7 @@ void	init_vars(t_data *data)
 	data->event.legend = 0;
 	data->event.actual_mode = 0;
 	data->event.actual_mesh.ref = NULL;
+	data->event.actual_mesh.type = 0;
 	data->event.actual_light = &data->spotlights[0];
 	data->event.bulb_color_sav = data->spotlights[0].color;
 	data->event.antia = 0;
