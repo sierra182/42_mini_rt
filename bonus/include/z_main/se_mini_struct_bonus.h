e1f303f444eabe4e0bdd5561ffd941de8dd7b38b
Dan SYLVAIN <dansylvain84@gmail.com>
2024-06-23 18:00:49 +0200

    engrenage file adapted to bigger resolution

--- bonus/include/z_main/se_mini_struct_bonus.h
+++ bonus/include/z_main/se_mini_struct_bonus.h
@@ -1,8 +1,8 @@
 #ifndef SE_MINI_STRUCT_BONUS_H
 # define SE_MINI_STRUCT_BONUS_H
 
-# define WIDTH 800
-# define HEIGHT 600
+# define WIDTH 1600
+# define HEIGHT 1200
 # define AXIS 3
 # define MTX 4 
 # define BIG_VALUE 100000000
